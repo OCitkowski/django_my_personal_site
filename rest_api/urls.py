db1b06a08a233dac9529a48dcf3483cc8ccd16d0
OCitkowski <o.citkowski@gmail.com>
2022-10-20 12:59:40 +0300

    add config and rest_api files

---
+++ rest_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from .views import TestView
+# from rest_api.views import TestApiView, CommentApiView
+
+app_name = 'blog_urls'
+urlpatterns = [
+    # ex: /blog
+    # path('', TestView.as_view(), name='home'),
+    # path('api/v1/test_list', TestApiView.as_view()),
+    # path('api/v1/comment_list', CommentApiView.as_view()),
+    # path('api/v1/comment_list/<int:pk>/', CommentApiView.as_view())
+]
