e2fafd4c65c893e5f84d88931eadce9b45208a3c
OCitkowski <o.citkowski@gmail.com>
2022-11-01 19:57:03 +0200

    reformatted views.py

--- home_page/views.py
+++ home_page/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView, TemplateView
 from basis_of_project.models import SiteConfiguration
 from basis_of_project.utils import MENU
 from django.utils import timezone
+from .models import Home
 
 
 class HomeView(ListView):
@@ -12,6 +13,7 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['home'] = Home.objects.filter(status='p')
         context['first'] = SiteConfiguration.objects.all().first()
         context['now'] = timezone.now()
         context['menu'] = MENU
