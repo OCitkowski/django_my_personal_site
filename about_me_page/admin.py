ad5e4528228fff2476071fac9706521a3e30992e
OCitkowski <o.citkowski@gmail.com>
2022-11-02 19:12:31 +0200

    added two models

--- about_me_page/admin.py
+++ about_me_page/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from solo.admin import SingletonModelAdmin
+from .models import TextAboutMe, PersonalInformation
 
-# Register your models here.
+class TextAboutMeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'text')
+    list_display_links = ('id', 'title', 'text')
+    search_fields = ('title',)
+
+
+admin.site.register(TextAboutMe, TextAboutMeAdmin)
+admin.site.register(PersonalInformation, SingletonModelAdmin)
--- about_me_page/models.py
+++ about_me_page/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from solo.models import SingletonModel
 
-# Create your models here.
+
+class PersonalInformation(SingletonModel):
+    name = models.CharField(max_length=255, default='-----')
+    age = models.CharField(max_length=255, default='-----')
+    residence = models.CharField(max_length=255, default='-----')
+    first_name = models.CharField(max_length=255, default='-----')
+    second_name = models.CharField(max_length=255, default='-----')
+
+    address = models.CharField(max_length=255, default='-----')
+    email = models.EmailField(max_length=255, default='-----')
+    phone = models.CharField(max_length=255, default='-----')
+    freelance = models.CharField(max_length=255, default='-----')
+
+    resume = models.FileField(upload_to='uploads/%Y/%m/%d/')
+
+    def __str__(self):
+        return "Personal Information"
+
+    class Meta:
+        verbose_name = "Personal Information"
+        verbose_name_plural = "Personal Information"
+
+
+class TextAboutMe(models.Model):
+    title = models.CharField(max_length=100, default='About Me.')
+    text = models.TextField(max_length=1000, default='-----')
+
+    def __str__(self):
+        return "About me"
+
+    class Meta:
+        verbose_name = "About me"
+        verbose_name_plural = "About me"
