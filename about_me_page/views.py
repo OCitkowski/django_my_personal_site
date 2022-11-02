633262349349e8a7f35b360986be150a6e2035fe
OCitkowski <o.citkowski@gmail.com>
2022-11-02 20:33:40 +0200

    modified views

--- about_me_page/views.py
+++ about_me_page/views.py
@@ -1,20 +1,17 @@
 from django.views.generic import ListView, DetailView, TemplateView
-from basis_of_project.models import SiteConfiguration
 from basis_of_project.utils import MENU
 from django.utils import timezone
-# from .models import About
+from .models import TextAboutMe, PersonalInformation
 
 
 class AboutView(ListView):
-
-    model = SiteConfiguration
+    model = TextAboutMe
     template_name = 'about_me_page/about.html'
     context_object_name = 'about'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['home'] = Home.objects.filter(status='p')
-        context['first'] = SiteConfiguration.objects.all().first()
+        context['personal_information'] = PersonalInformation.objects.all().first()
         context['now'] = timezone.now()
         context['menu'] = MENU
         return context
