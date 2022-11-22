b56f28341a037f985369bf00adc903639c1b3d89
OCitkowski <o.citkowski@gmail.com>
2022-11-22 19:04:01 +0200

    refactor the template contact page

--- basis_of_project/utils.py
+++ basis_of_project/utils.py
@@ -1,8 +1,8 @@
 MENU = [{'title': "Home", 'url_name': '/home'},
         {'title': "AboutMe", 'url_name': '/aboutme'},
         {'title': "Resume", 'url_name': '/resume'},
-        {'title': "Portfolio", 'url_name': '/portfolio'},
-        {'title': "Blog", 'url_name': '/blogs'},
+        # {'title': "Portfolio", 'url_name': '/portfolio'},
+        # {'title': "Blog", 'url_name': '/blogs'},
         {'title': "Contact", 'url_name': '/contact'},
 ]
 
