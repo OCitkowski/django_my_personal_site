626c18ae1103a7800946aae056962503d0f31458
OCitkowski <o.citkowski@gmail.com>
2022-11-22 20:21:31 +0200

    added test for home page / Home models

--- home_page/tests.py
+++ home_page/tests.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
 from django.test import TestCase
+from home_page.models import Home
+from basis_of_project.utils import STATUS_CHOICES
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Home.objects.create(title_en="lion", date_added=datetime.now(), date_update=datetime.now(), status=STATUS_CHOICES[1])
+
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Home.objects.get(title_en="lion")
+        self.assertEqual(lion.title_en, "lion4")
+
 
 # Create your tests here.
