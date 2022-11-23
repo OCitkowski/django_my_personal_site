e70dcfa0603af02b4706fe186be11e123eaeb5a9
OCitkowski <o.citkowski@gmail.com>
2022-11-23 10:56:44 +0200

    v 1.0

--- basis_of_project/views.py
+++ basis_of_project/views.py
@@ -16,14 +16,4 @@ def custom_page_not_found_view(request, exception):
 # def custom_bad_request_view(request, exception=None):
 #     return render(request, "basis_of_project/errors/400.html", {})
 
-#
-# def handler404(request, exception):
-#     context = {}
-#     response = render(request, "pages/errors/404.html", context=context)
-#     response.status_code = 404
-#     return response
-# def handler500(request):
-#     context = {}
-#     response = render(request, "pages/errors/500.html", context=context)
-#     response.status_code = 500
-#     return response
+
