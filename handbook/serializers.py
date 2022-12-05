abf0fbf5c7de1dcc4da9730a208fd0b0518a3bf0
fox <o.citkowski@gmail.com>
2022-12-05 12:41:19 +0100

    to added count_note to NoteSerializer

--- handbook/serializers.py
+++ handbook/serializers.py
@@ -23,7 +23,10 @@ class NoteSerializer(serializers.ModelSerializer):
     date_update = serializers.DateField(format="%Y-%m-%d", input_formats=['%Y-%m-%d', ])
     ownername = serializers.ReadOnlyField(source='owner.name')
     sourcename = serializers.ReadOnlyField(source='source.name')
-
+    count_note = serializers.SerializerMethodField()
     class Meta:
         model = Note
-        fields = ('id', 'owner', 'ownername', 'source', 'sourcename', 'comment', 'status', 'text', 'date_update')
+        fields = ('id', 'owner', 'ownername', 'source', 'sourcename', 'comment', 'status', 'text', 'date_update', 'count_note')
+
+    def get_count_note(self, instance):
+        return Note.objects.all().count()
