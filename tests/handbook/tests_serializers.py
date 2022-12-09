4fe54bc6ba8fd496bafc60ca6d059c1e7bf9600a
fox <o.citkowski@gmail.com>
2022-12-09 12:39:52 +0100

    +

--- tests/handbook/tests_serializers.py
+++ tests/handbook/tests_serializers.py
@@ -14,6 +14,7 @@ class SourceSerializerTestCase(APITestCase):
         # print(response.data)
         # self.owner1 = Owner.objects.create(name='Test1', comment='test_comment1')
 
+
         # url = 'http://127.0.0.1:8000/api/v1/'
         # headers = {'Authorization': 'Token 9944b09199c62bcf9418ad846dd0e4bbdfc6ee4b'}
         # response = requests.get(url, headers=headers)
