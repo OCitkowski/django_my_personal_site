16325f179f905d81fa474fd24905a8db878e1f18
OCitkowski <o.citkowski@gmail.com>
2022-12-20 17:56:30 +0200

    added in post_all_data.py +001

--- basis_of_project/management/commands/post_all_data.py
+++ basis_of_project/management/commands/post_all_data.py
@@ -7,45 +7,40 @@ from about_me_page.models import TextAboutMe, PersonalInformation, Services
 from resume_page.models import Skills, Education, Experience
 
 from basis_of_project.utils import STATUS_CHOICES
+import sqlite3
 
-# locale_list = ['en-US', 'uk-UA', 'de-DE']
-locale_list = ['en-US']
-model_names = ['all', 'Home', 'TextAboutMe', 'PersonalInformation', 'Services', 'Skills', 'Education', 'Experience']
+models = [Home, TextAboutMe, PersonalInformation, Services, Skills, Education, Experience]
 
 
 class Command(BaseCommand):
     # https: // docs.djangoproject.com / en / 4.1 / howto / custom - management - commands /
     help = 'Fill'
 
-    def add_arguments(self, parser):
-        parser.add_argument('model_name', nargs='+', type=str)
+    #
+    # def add_arguments(self, parser):
+    #     parser.add_argument('model_name', nargs='+', type=str)
 
-    def fill_home(self):
-        titles = ['Django developer', 'full stack developer ']
+    def get_data_from_old(self, table_name):
+        con = sqlite3.connect("copySQLite/db.sqlite3")
+        cursor = con.cursor()
+        cursor.execute(f"SELECT * FROM {table_name}")
+        sql_data = cursor.fetchall()
+        return sql_data
 
-        for title in titles:
+    def fill_data(self, model):
+        old_data = self.get_data_from_old('home_page_home')
+        for row in old_data:
             try:
-                add_home = Home(title_en=title, status=STATUS_CHOICES[1])
-                add_home.save()
-                print('Hello')
+                if model.__name__ == 'Home':
+                    add_new = model(title_en=row[1], status=row[4])
+                    add_new.save()
+                    print(row)
             except:
                 pass
 
     def handle(self, *args, **options):
-        if options["model_name"][0] in model_names:
-            self.fill_home()
+        # if options["model_name"][0] in model_names:
+        for model in models:
+            self.fill_data(model)
 
         self.stdout.write(self.style.SUCCESS('Successfully created models'))
-
-    # def fill_TextAboutMe(self):
-    #     titles = ['Django developer', 'full stack developer ']
-    #
-    #     for title in titles:
-    #         try:
-    #             add_home = Home(title=title, text=STATUS_CHOICES[1])
-    #             title = models.CharField(max_length=100, default='About Me.')
-    #             text = models.TextField(max_length=1000, default='-----')
-    #             add_home.save()
-    #             print('Hello')
-    #         except:
-    #             pass
