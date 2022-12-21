7b2e1f56b317dedfde1e87873a5e55703d2b3922
OCitkowski <o.citkowski@gmail.com>
2022-12-21 13:38:39 +0200

    added in post_all_data.py +002

--- basis_of_project/management/commands/post_all_data.py
+++ basis_of_project/management/commands/post_all_data.py
@@ -9,38 +9,79 @@ from resume_page.models import Skills, Education, Experience
 from basis_of_project.utils import STATUS_CHOICES
 import sqlite3
 
-models = [Home, TextAboutMe, PersonalInformation, Services, Skills, Education, Experience]
+models = {Home: 'home_page_home',
+          TextAboutMe: 'about_me_page_textaboutme',
+          PersonalInformation: 'about_me_page_personalinformation',
+          Services: 'about_me_page_services',
+          Skills: 'resume_page_skills',
+          Education: 'resume_page_education',
+          Experience: 'resume_page_experience',
+          }
 
 
 class Command(BaseCommand):
     # https: // docs.djangoproject.com / en / 4.1 / howto / custom - management - commands /
     help = 'Fill'
 
-    #
-    # def add_arguments(self, parser):
-    #     parser.add_argument('model_name', nargs='+', type=str)
-
     def get_data_from_old(self, table_name):
+
         con = sqlite3.connect("copySQLite/db.sqlite3")
         cursor = con.cursor()
         cursor.execute(f"SELECT * FROM {table_name}")
         sql_data = cursor.fetchall()
         return sql_data
 
-    def fill_data(self, model):
-        old_data = self.get_data_from_old('home_page_home')
+    def fill_data(self, model, old_data):
         for row in old_data:
             try:
                 if model.__name__ == 'Home':
                     add_new = model(title_en=row[1], status=row[4])
                     add_new.save()
-                    print(row)
-            except:
-                pass
+                    print(row, model.__name__)
+
+                if model.__name__ == 'TextAboutMe':
+                    add_new = model(text=row[1], title=row[2])
+                    add_new.save()
+                    print(row, model.__name__)
+
+                if model.__name__ == 'PersonalInformation':
+                    add_new = model(address=row[1], age=row[2], email=row[3], first_name=row[4], freelance=row[5],
+                                    name=row[6], phone=row[7], residence=row[8], resume=row[9], second_name=row[10])
+                    add_new.save()
+                    print(row, model.__name__)
+
+                if model.__name__ == 'Services':
+                    add_new = model(icon_text=row[1], text=row[2], title=row[3])
+                    add_new.save()
+                    print(row, model.__name__)
+
+                if model.__name__ == 'Skills':
+                    add_new = model(level=row[2], status=row[3], title=row[1])
+                    add_new.save()
+                    print(row, model.__name__)
+
+                if model.__name__ == 'Education':
+                    add_new = model(date_end=row[5], date_start=row[4], place=row[2], status=row[6], text=row[3],
+                                    title=row[1])
+                    add_new.save()
+                    print(row, model.__name__)
+
+                if model.__name__ == 'Experience':
+                    add_new = model(company=row[2], date_end=row[5], date_start=row[4], status=row[6], text=row[3],
+                                    title=row[1])
+                    add_new.save()
+                    print(row, model.__name__)
+
+
+            except Exception as inst:
+                print(type(inst))  # the exception instance
+                print(inst.args)  # arguments stored in .args
+                print(inst)  # __str__ allows args to be printed directly,
+                print(f'{model.__name__} failed ')
 
     def handle(self, *args, **options):
-        # if options["model_name"][0] in model_names:
-        for model in models:
-            self.fill_data(model)
+        for model, table_name in models.items():
+            old_data = self.get_data_from_old(table_name)
+            self.fill_data(model, old_data)
 
         self.stdout.write(self.style.SUCCESS('Successfully created models'))
--- config/urls.py
+++ config/urls.py
@@ -12,12 +12,12 @@ handler404 = 'basis_of_project.views.custom_page_not_found_view'
 
 urlpatterns = [
     path('api/token_auth/', views.obtain_auth_token, name='token_auth'),
-    path('api/v1/', include('rest_api.urls')),
-    path('', include('home_page.urls'), name='api_v1'),
+    path('api/v1/', include('rest_api.urls'), name='api_v1'),
+    path('home/', include('home_page.urls')),
+    path('', include('home_page.urls')),
     path('aboutme/', include('about_me_page.urls')),
     path('resume/', include('resume_page.urls')),
     path('contact/', include('contact_page.urls')),
-    # path('api-auth/', include('rest_api.urls')),
     path('admin/', admin.site.urls),
 
 ]
